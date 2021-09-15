9df6dff5077f2fe476dbe3c597f14953c4c65a5f
ttommyth <ttommyth@gmail.com>
2021-09-15 22:46:33 +0800

    Update App.tsx
    added googleads

--- bonk-to-website/src/App.tsx
+++ bonk-to-website/src/App.tsx
@@ -22,12 +22,12 @@ const App= ( )=> {
   }
   return (
     <div className="App">
-      bonked: {counter}
       <div onMouseDown={doBonk} onMouseUp={doUnbonk} className={"disableSelection"}
       onDragStart={e=>e.preventDefault()}
         style={{ height: "100%", width: "100%", alignContent: "center", alignItems: "center" }}
       >
         <div className={"bonkerContainer"}>
+      bonked: {counter}
           <img src={Bonker} alt="Bonker" className={"bonker"} />
           <div className={"bonkstick" + (bonk ? " bonked" : "")}>
             <img src={BonkStick} alt="BonkStick" />
@@ -39,6 +39,20 @@ const App= ( )=> {
           <img src={Bonkee} alt="Bonkee" className={"bonkee" + (bonk ? " bonked" : "")} />
           <img src={BonkeePostBonk} alt="BonkeePostBonk" className={"postbondbonkee" + (bonk ? " bonked" : "")} />
         </div>
+      </div>
+      <div style={{backgroundColor:"#696969", width:"100%", height: "8%"}}>
+
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8517193754882771"
+     crossOrigin="anonymous"></script>
+<ins className={"adsbygoogle"}
+     style={{display:"block"}}
+     data-ad-client="ca-pub-8517193754882771"
+     data-ad-slot="5662747107"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
       </div>
     </div>
   );
